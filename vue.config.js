40df6082191b1f63b1c1e05dcc946c15a8f556d4
Arjen de Jong <arjen@mediasoep.nl>
2020-01-02 20:08:01 +0100

    Add Vuetify

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  transpileDependencies: ["vuetify"],
+  productionSourceMap: false,
+  filenameHashing: false,
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    }
+  },
+  chainWebpack: config => {
+    config.externals({
+      ...config.get("externals"),
+      moment: "moment"
+    });
+  }
+};
